47be8d6a70bb5b23c143818c8e76a452b7fd09b8
Brian Takita <brian.takita@gmail.com>
2021-06-26 23:07:17 -0400

    ## 6.0.9
    ### Patch Changes
    
    - dist directory

--- src/self_lazyload.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function self_lazyload<Val extends unknown = unknown>(self: object, version: string, cache_symbol: string, version_symbol: string, $value: () => Val): any;
-export { self_lazyload as lazyload__self };
